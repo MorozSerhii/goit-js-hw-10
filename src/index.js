1a91af148da618f04ab6231a95cd5cbd98b3b1e8
Serhii <ecprap@gmail.com>
2023-02-12 15:52:00 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
+
+const listEl = document.querySelector('.country-list');
+const inputElement = document.querySelector('#search-box');
+const cardContainerEl = document.querySelector('.country-info');
+inputElement.addEventListener('input', debounce(dataSent, DEBOUNCE_DELAY));
+
+function dataSent() {
+  let name = inputElement.value;
+
+  if (!name) {
+    return;
+  }
+
+  fetchCountries(name)
+    .then(data => {
+      if (data.status === 404) {
+        throw new Error(data.status);
+      }
+
+      if (data.length === 1) {
+        let countryInfo = data.map(data => createCountryCard(data));
+        listEl.innerHTML = '';
+        return (cardContainerEl.innerHTML = countryInfo);
+      }
+      if (data.length <= 10) {
+        let allCauntry = data.reduce(
+          (markup, data) => createMarkup(data) + markup,
+          ''
+        );
+        cardContainerEl.innerHTML = '';
+        return (listEl.innerHTML = allCauntry);
+      }
+
+      Notify.info('Too many matches found. Please enter a more specific name.');
+      return;
+    })
+    .catch(onError);
+  listEl.innerHTML = '';
+  cardContainerEl.innerHTML = '';
+}
+
+function createMarkup({ name, flags }) {
+  return `<li class="list-item" >
+    <img src=${flags.svg} class="list-img" />
+    <h2 class="article-title">${name.common}</h2>
+    
+  </li>
+  `;
+}
+
+function createCountryCard({ name, capital, population, flags, languages }) {
+  return `<li class="list-item" >
+    <img src=${flags.svg} class="list-img" />
+    <h2 class="article-title">${name.common}</h2>
+    <span>Capital:${capital}</span>
+    <span>Population:${population}</span>
+    <span>Languages:${Object.values(languages)}</span>
+    
+  </li>
+  `;
+}
+
+function onError() {
+  Notify.failure('Oops, there is no country with that name');
+  console.error('no country with that name');
+}
